e0a32d5a98d73108316924957433a9b05a711479
Dilvir134 <dilvir134@gmail.com>
2024-11-02 16:02:09 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Welcome to Fullstack Development - I</h2>
+        <h3>React JS Programming Week09 Lab exercise</h3>
+        <h4>101398345</h4>
+        <h5>Dilvir Singh</h5>
+        <p>George Brown College, Toronto</p>
+    </div>
+  );
+}
+
+export default App;
